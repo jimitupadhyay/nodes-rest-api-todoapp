32380844f673411e7ba3405e1d6fe6d15cc03ca2
Jimit Upadhyay <jimitupadhyay@gmail.com>
2018-08-30 17:59:45 -0400

    Added GET /todos route

--- server/server.js
+++ server/server.js
@@ -23,6 +23,14 @@ app.post('/todos',(req, res) => {
 
 });
 
+app.get('/todos',(req, res) =>{
+    Todo.find().then( (result) => {
+        res.status(200).send({result});
+    }, (err) => {
+        res.status(400).send(e);
+    });
+});
+
 app.listen(3000,() => {
     console.log('Server is running on PORT:3000');
 });
