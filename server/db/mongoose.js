cac29c550f417352cd6854c1eedeb7132e966e4f
Jimit Upadhyay <jimitupadhyay@gmail.com>
2018-08-31 01:08:29 -0400

    DB ERROR

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://mongodb_jimitupadhyay:mongodb_jimitupadhyay@ds239682.mlab.com:39682/mongodb_jimitupadhyay'|| 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI|| 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
